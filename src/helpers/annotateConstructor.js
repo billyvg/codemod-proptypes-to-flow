89181cdd4cfee119326d2fc094028b8608aea237
Billy Vong <billy@opstarts.com>
2016-04-21 16:51:37 -0700

    Add helper to annotate constructor param `props` and class member `props`

---
+++ src/helpers/annotateConstructor.js
@@ -0,0 +1,30 @@
+/**
+ * Annotates ES2015 Class constructor and Class `props` member
+ *
+ * @param {jscodeshiftApi} j jscodeshift API
+ * @param {Array} body Array of `Node`
+ */
+export default function annotateConstructor(j, body) {
+  let constructorIndex;
+  const typeAnnotation = j.typeAnnotation(
+    j.genericTypeAnnotation(j.identifier('Props'), null)
+  );
+
+  body.some((b, i) => {
+    if (b.kind === 'constructor') {
+      constructorIndex = i + 1;
+
+      // first parameter is always props regardless of name
+      if (b.value.params && b.value.params.length) {
+        b.value.params[0].typeAnnotation = typeAnnotation;
+      }
+      return true;
+    }
+  });
+
+  body.splice(
+    constructorIndex,
+    0,
+    j.classProperty(j.identifier('props'), null, typeAnnotation)
+  );
+}
