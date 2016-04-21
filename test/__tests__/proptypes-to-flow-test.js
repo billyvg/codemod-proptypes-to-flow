4a94b9fd2a10d00cf0b4af42281ee12b33653eae
Billy Vong <billy@opstarts.com>
2016-04-21 13:42:50 -0700

    Add tests

--- test/__tests__/proptypes-to-flow-test.js
+++ test/__tests__/proptypes-to-flow-test.js
@@ -1,5 +1,21 @@
 describe('React.PropTypes to flow', () => {
-  it('transforms correctly', () => {
-    test('index', 'proptypes-to-flow-test');
+  it('transforms optional PropTypes prefixed with `React`', () => {
+    test('simple-transform', 'react-optional-proptypes');
+  });
+
+  it('transforms required PropTypes prefixed with `React`', () => {
+    test('simple-transform', 'react-required-proptypes');
+  });
+
+  it('transforms optional PropTypes with no `React` prefix', () => {
+    test('simple-transform', 'optional-proptypes');
+  });
+
+  it('transforms required PropTypes with no `React` prefix', () => {
+    test('simple-transform', 'required-proptypes');
+  });
+
+  it('transforms PropTypes with comments', () => {
+    test('simple-transform', 'proptypes-with-comments');
   });
 });
