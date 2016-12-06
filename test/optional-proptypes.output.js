23c9dbe63dcd0cba151298d6d50543c2b2499048
skovhus <kenneth.skovhus@gmail.com>
2016-12-06 10:13:32 +0100

    Fix dependencies and build

--- test/optional-proptypes.output.js
+++ test/optional-proptypes.output.js
@@ -1,19 +1,19 @@
 type Props = {
-  optionalArray?: Array<any>;
-  optionalBool?: boolean;
-  optionalFunc?: Function;
-  optionalNumber?: number;
-  optionalObject?: Object;
-  optionalString?: string;
-  optionalNode?: number | string | React.Element | Array<any>;
-  optionalElement?: React.Element;
-  optionalMessage?: Message;
-  optionalEnum?: "News" | "Photos";
-  optionalUnion?: string | number | Message;
-  optionalArrayOf?: Array<number>;
-  optionalObjectOf?: Object<number>;
+  optionalArray?: Array<any>,
+  optionalBool?: boolean,
+  optionalFunc?: Function,
+  optionalNumber?: number,
+  optionalObject?: Object,
+  optionalString?: string,
+  optionalNode?: number | string | React.Element | Array<any>,
+  optionalElement?: React.Element,
+  optionalMessage?: Message,
+  optionalEnum?: "News" | "Photos",
+  optionalUnion?: string | number | Message,
+  optionalArrayOf?: Array<number>,
+  optionalObjectOf?: Object<number>,
   optionalObjectWithShape?: {
-    color?: string;
-    fontSize?: number;
-  };
+    color?: string,
+    fontSize?: number,
+  },
 };
--- test/react-required-proptypes.output.js
+++ test/react-required-proptypes.output.js
@@ -1,19 +1,19 @@
 type Props = {
-  requiredArray: Array<any>;
-  requiredBool: boolean;
-  requiredFunc: Function;
-  requiredNumber: number;
-  requiredObject: Object;
-  requiredString: string;
-  requiredNode: number | string | React.Element | Array<any>;
-  requiredElement: React.Element;
-  requiredMessage: Message;
-  requiredEnum: "News" | "Photos";
-  requiredUnion: string | number | Message;
-  requiredArrayOf: Array<number>;
-  requiredObjectOf: Object<number>;
+  requiredArray: Array<any>,
+  requiredBool: boolean,
+  requiredFunc: Function,
+  requiredNumber: number,
+  requiredObject: Object,
+  requiredString: string,
+  requiredNode: number | string | React.Element | Array<any>,
+  requiredElement: React.Element,
+  requiredMessage: Message,
+  requiredEnum: "News" | "Photos",
+  requiredUnion: string | number | Message,
+  requiredArrayOf: Array<number>,
+  requiredObjectOf: Object<number>,
   requiredObjectWithShape: {
-    color: string;
-    fontSize: number;
-  };
+    color: string,
+    fontSize: number,
+  },
 };
