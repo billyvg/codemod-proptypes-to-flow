c36e354351eedd1c3e7e58ddd52f4a235b201374
Tomasz Sodzawiczny <tomasz.sodzawiczny@brainly.com>
2018-02-20 14:51:45 +0100

    Add --flowComment option

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import transformEs6ClassComponents from './transformers/es6Classes';
 import transformFunctionalComponents from './transformers/functional';
 import ReactUtils from './helpers/ReactUtils';
 
-function addFlowComment(j, ast) {
+function addFlowComment(j, ast, options) {
   const getBodyNode = () => ast.find(j.Program).get('body', 0).node;
 
   const comments = getBodyNode().comments || [];
@@ -10,15 +10,40 @@ function addFlowComment(j, ast) {
     comments.filter(e => e.value.indexOf('@flow') !== -1).length > 0;
 
   if (!containsFlowComment) {
+    function unshiftFlowCommentLine() {
+      comments.unshift(j.commentLine(' @flow'));
+    }
+  
+    function unshiftFlowCommentBlock() {
     comments.unshift(j.commentBlock(' @flow '));
+  }
+
+    switch (options.flowComment) {
+      case 'line':
+        unshiftFlowCommentLine(comments);
+        break;
+      case 'block':
+        unshiftFlowCommentBlock(comments);
+        break;
+    }
   }
 
   getBodyNode().comments = comments;
 }
 
-export default function transformer(file, api) {
+export default function transformer(file, api, rawOptions) {
   const j = api.jscodeshift;
   const root = j(file.source);
+
+  const options = rawOptions;
+  if (options.flowComment !== 'line' && options.flowComment !== 'block') {
+    if (options.flowComment) {
+      console.warn('Unsupported flowComment value provided: ${options.flowComment}.');
+      console.warn('Supported options are "block" and "line".');
+      console.warn('Falling back to default: "block".');
+    }
+    options.flowComment = 'block';
+  }
 
   const reactUtils = ReactUtils(j);
   if (!reactUtils.hasReact(root)) {
@@ -29,7 +54,7 @@ export default function transformer(file, api) {
   const functionalModifications = transformFunctionalComponents(root, j);
 
   if (classModifications || functionalModifications) {
-    addFlowComment(j, root);
+    addFlowComment(j, root, options);
     return root.toSource({ quote: 'single', trailingComma: true });
   } else {
     return file.source;
